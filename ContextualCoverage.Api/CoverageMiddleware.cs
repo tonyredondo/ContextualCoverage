379edcf5fe21fa84a8e3e24e5d87c0724e574851
Daniel Redondo <tonyredondo@gmail.com>
2020-01-28 23:09:16 +0100

    coverage server support

--- ContextualCoverage.Api/CoverageMiddleware.cs
+++ ContextualCoverage.Api/CoverageMiddleware.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using ScopeAgent.Coverage;
+using ScopeAgent.Coverage.Attributes;
 
 namespace ContextualCoverage.Api
 {
@@ -14,13 +19,14 @@ namespace ContextualCoverage.Api
     {
         private readonly RequestDelegate _next;
         private readonly ILogger _logger;
-        private int _coverageSession;
+        private readonly HttpClient _client;
 
         public CoverageMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
             CoverageReporter.Handler = new ScopeCoverageEventHandler(new CoverageSettings());
             _next = next;
             _logger = loggerFactory.CreateLogger<CoverageMiddleware>();
+            _client = new HttpClient();
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -37,12 +43,50 @@ namespace ContextualCoverage.Api
             CoverageReporter.Handler.StartAsyncSession(Guid.NewGuid());
             await _next(context).ConfigureAwait(false);
             var session = CoverageReporter.Handler.EndAsyncSession();
-            if (session != null && Interlocked.CompareExchange(ref _coverageSession, 1, 0) == 0)
+            if (session != null)
             {
                 _logger.LogInformation("Saving coverage info");
-                var json = JsonConvert.SerializeObject(session, Formatting.Indented);
+                var covPayload = new CoverageServerPayload(session);
+                var json = JsonConvert.SerializeObject(covPayload, Formatting.Indented);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var result = await _client.PostAsync("http://rotopia.xyz:8080/ingest", content).ConfigureAwait(false);
                 File.WriteAllText($"coverage.json", json);
             }
+        }
+
+        public class CoverageServerPayload
+        {
+            [DataMember(Name = "gitInfo")]
+            public CoverageServerGitInfo GitInfo { get; set; }
+            [DataMember(Name = "coverage")]
+            public object Coverage { get; set; }
+
+            public CoverageServerPayload(object coverage)
+            {
+                var covAttribute = Assembly.GetEntryAssembly().GetCustomAttribute<CoveredAssemblyAttribute>();
+                if (covAttribute != null)
+                {
+                    GitInfo = new CoverageServerGitInfo
+                    {
+                        Repository = covAttribute.Repository,
+                        Branch = covAttribute.Branch,
+                        Commit = covAttribute.Commit,
+                        SourceRoot = covAttribute.SourceRoot
+                    };
+                }
+                Coverage = coverage;
+            }
+        }
+        public class CoverageServerGitInfo
+        {
+            [DataMember(Name = "repository")]
+            public string Repository { get; set; }
+            [DataMember(Name = "branch")]
+            public string Branch { get; set; }
+            [DataMember(Name = "commit")]
+            public string Commit { get; set; }
+            [DataMember(Name = "sourceRoot")]
+            public string SourceRoot { get; set; }
         }
     }
 }
